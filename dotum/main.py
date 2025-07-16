4cdaa56ca0307bad8c8d55fbf0b4374895a2da19
Henrique Sebastião <contato@henriquesebastiao.com>
2025-07-15 20:00:30 -0400

    📜 docs: Update description on main.py

--- dotum/main.py
+++ dotum/main.py
@@ -13,17 +13,29 @@ def custom_generate_unique_id(route: APIRoute):
 settings = get_settings()
 
 description = f"""
-Dotum
+[![CI](https://github.com/henriquesebastiao/dotum/actions/workflows/test.yml/badge.svg)](https://github.com/henriquesebastiao/dotum/actions/workflows/test.yml)
+[![coverage](https://coverage-badge.samuelcolvin.workers.dev/henriquesebastiao/dotum.svg)](https://coverage-badge.samuelcolvin.workers.dev/redirect/henriquesebastiao/dotum)
+[![fastapi](https://img.shields.io/badge/FastAPI-009688?style=flat&logo=fastapi&logoColor=white)](https://fastapi.tiangolo.com/)
+[![postgresql](https://img.shields.io/badge/PostgreSQL-4169E1?style=flat&logo=postgresql&logoColor=white)](https://www.postgresql.org/)
+[![docker](https://img.shields.io/badge/Docker-2496ED?style=flat&logo=docker&logoColor=white)](https://www.docker.com/)
+
 #### Documentação alternativa: [Redoc]({settings.APP_URL}/redoc)
+
+Este projeto é uma solução para um desafio de programação back-end,
+cujo objetivo é desenvolver uma aplicação para o controle de contas a pagar e contas a receber.
+A proposta foca na construção de uma lógica sólida,
+estrutura de código bem organizada e cumprimento dos requisitos funcionais.
+
+Veja o código fonte deste projeto no GitHub [henriquesebastiao/dotum](https://github.com/henriquesebastiao/dotum/).
 """
 
 app = FastAPI(
     docs_url='/',
     generate_unique_id_function=custom_generate_unique_id,
-    title='Dotum',
+    title='Dotum - Sistema de contas a pagar e contas a receber',
     description=description,
     version=settings.VERSION,
-    terms_of_service='https://github.com/henriquesebastiao/dotum/',
+    terms_of_service='https://github.com/henriquesebastiao/dotum/blob/main/LICENSE',
     contact={
         'name': 'Dotum',
         'url': 'https://github.com/henriquesebastiao/dotum',
